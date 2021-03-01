3334092c62ca2162eb727f9e75ab2e67961a6274
yousrasrk <39521130+yousrasrk@users.noreply.github.com>
2021-03-01 11:52:03 +0100

    Create home.php

---
+++ views/home.php
@@ -0,0 +1,71 @@
+<?php include 'views/includes/header.php';?>
+<?php include 'views/includes/header1.php';?>
+<div class="box">
+    <div class="container" style="margin-top:120px;">
+ 
+     	<div class="row" >
+			 
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" >
+                <a href="http://localhost/woocommercePhPApp/?page=product/list" style="  text-decoration: none;">
+					<div class="box-part text-center" style="color:#fff; background-color:#ff9933;">
+                        
+                        <i class="fab fa-product-hunt fa-3x" aria-hidden="true"></i>
+                        
+						<div class="title">
+							<h4>Gestion des Produits</h4>
+						</div>
+                        
+						<div class="text">
+							<span></span>
+						</div>
+                        
+                        
+                     </div>
+                </a> 
+            </div>	 
+            
+
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" >
+                <a href="http://localhost/woocommercePhPApp/?page=customer/list" style=" color:#fff; text-decoration: none;">
+					<div class="box-part text-center" style="color:#fff; background-color:#9932CC;">
+                        
+                        <i class="fa fa-user fa-3x" style="color:#fff;" aria-hidden="true"></i>
+                        
+						<div class="title">
+							<h4>Gestion des clients</h4>
+						</div>
+                        
+						<div class="text">
+							<span></span>
+						</div>
+                        
+                        
+                     </div>
+                </a> 
+		    </div>	 
+                
+            
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" >
+                <a href="http://localhost/woocommercePhPApp/?page=order/list" style="  text-decoration: none;">
+					<div class="box-part text-center" style="color:#fff; background-color:#00008B;">
+                        
+                        <i class="fa fa-shopping-cart fa-3x" style="color:#fff;"aria-hidden="true"></i>
+                        
+						<div class="title">
+							<h4>Gestion des Commandes</h4>
+						</div>
+                        
+						<div class="text">
+							<span></span>
+						</div>
+                        
+                        
+                     </div>
+                </a> 
+		    </div>	 
+				
+				
+				 
+		</div>		
+    </div>
+</div>
